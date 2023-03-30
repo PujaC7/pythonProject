d6297d3a025f7054daaa6e221400433f5c70a707
PujaC7 <chowdhury4330@gmail.com>
2023-03-30 14:26:48 +0530

    reverse string #3

--- String_to_lower.py
+++ String_to_lower.py
@@ -1,5 +1,5 @@
-name = input("enter the original string: ")
+name= input("enter the original string is: ")
 print("original string is: ")
 print(name)
 print("\n converted string is: ")
-print(name.lower())
+print(name.lower()) #lower function used to convert string to lower case
---
+++ reverse_string.py
@@ -0,0 +1,4 @@
+name = input("enter the string is: ")
+print(name)
+print("\n the reverse string is: ")
+print(name.join())
